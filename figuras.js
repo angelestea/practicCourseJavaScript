6acf2da9aee8950c5a77385ee8ad2e9b876f0edc
Angel Vega <angelhidrobo2015@gmail.com>
2022-04-06 10:01:48 -0500

    setting code to modularization mode with function

--- figuras.js
+++ figuras.js
@@ -1,63 +1,90 @@
 
 //Código del cuadrado
 console.group("Cuadrado");
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+//valores constantes
+// const ladoCuadrado = 5;
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
+function perimetroCuadrado(lado){
+    return lado * 4;
+};
+// console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El área del cuadrado es: " + areaCuadrado + " cm ^2");
+//const areaCuadrado = ladoCuadrado * ladoCuadrado;
+// console.log("El área del cuadrado es: " + areaCuadrado + " cm ^2");
+
+function areaCuadrado(lado){
+    return lado * lado;
+};
+
 console.groupEnd();
 
 //Código del trángulo
 console.group("Triangulo");
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
+//valores constantes
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// const alturaTriangulo = 5.5;
 
-console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cm, " + ladoTriangulo2 + " cm, " + baseTriangulo + " cm");
-console.log("La altura del triángulo es de: " + alturaTriangulo + " cm");
+// console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cm, " + ladoTriangulo2 + " cm, " + baseTriangulo + " cm");
+// console.log("La altura del triángulo es de: " + alturaTriangulo + " cm");
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
+//const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+function perimetroTriangulo(lado1, lado2, lado3){
+    return lado1 + lado2 + lado3;
+}
+//console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
 
-const areaTriangulo = ( baseTriangulo * alturaTriangulo ) / 2;
-console.log("El área del triángulo es: " + areaTriangulo + " cm ^2");
+// const areaTriangulo = ( baseTriangulo * alturaTriangulo ) / 2;
+// console.log("El área del triángulo es: " + areaTriangulo + " cm ^2");
+
+function triangleArea(base, altura){
+    return ( base * altura ) / 2;
+}
 
 console.groupEnd();
 
-//Circulo
-
+//Código del circulo
+//valores constantes
 console.group("Círculo");
 
 //Radio
-
-const radioCirculo = 4;
-console.log("El radio del círculo es: " + radioCirculo + " cm");
+//valores constantes
+// const radioCirculo = 4;
+// console.log("El radio del círculo es: " + radioCirculo + " cm");
 
 //Diámetro
-
-const diametroCirculo = radioCirculo * 2;
-console.log("El diámetro del círculo es: " + diametroCirculo + " cm");
+function circleDiameter(radio){
+    return radio * 2;
+}
+//valores constantes
+// const diametroCirculo = radioCirculo * 2;
+// console.log("El diámetro del círculo es: " + diametroCirculo + " cm");
 
 //pi
 
-//const pi = 3.1415;
-const pi = Math.PI;
-console.log("Pi es: " + pi + " cm");
+const pi1 = 3.1415;
+const pi2 = Math.PI;
+//console.log("Pi es: " + pi2 + " cm");
 
 // Circunferencia
 
-const perimetroCirculo = diametroCirculo * pi;
-console.log("La circunferencia del círculo es: " + perimetroCirculo + " cm");
+function circumferenceCircle(radio){
+    return circleDiameter(radio) * pi2;
+}
+
+//const perimetroCirculo = diametroCirculo * pi;
+//console.log("La circunferencia del círculo es: " + perimetroCirculo + " cm");
 
 
 //Área
+//valores constantes
 
-const areaCirculo = (radioCirculo * radioCirculo) * pi;
-console.log("El área del círculo es: " + areaCirculo + " cm^2");
+// const areaCirculo = (radioCirculo * radioCirculo) * pi;
+// console.log("El área del círculo es: " + areaCirculo + " cm^2");
+function circleArea(radio){
+    return (radio * radio) * pi2;
+}
 
 console.groupEnd();
