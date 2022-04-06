cb114d5f194bd921640202ea0f1f398f9b0565b7
Angel Vega <angelhidrobo2015@gmail.com>
2022-04-06 11:00:35 -0500

    first work completed

--- figuras.js
+++ figuras.js
@@ -55,3 +55,22 @@ function circleArea(radio){
 }
 
 console.groupEnd();
+
+// Interacción con html
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
