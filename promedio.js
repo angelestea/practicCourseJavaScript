9be5510560d6c48c6959f62c2695ec0cfc885f6b
Angel Vega <angelhidrobo2015@gmail.com>
2022-04-06 21:53:20 -0500

    saving

--- promedio.js
+++ promedio.js
@@ -1,24 +1,20 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    500,
-];
-
-let sumaLista1 = 0;
-
-for(let i = 0; i < lista1.length; i++){
-    sumaLista1 += lista1[i];
-}
-
-const promedio = sumaLista1 / lista1.length;
-
-
 function calcularMediaAritmetica(lista){
-    let
-    for(let i = 0; i < lista1.length; i++){
-        sumaLista1 += lista1[i];
-    }
     
-    const promedio = sumaLista1 / lista1.length;
+
+    // let sumalista = 0;
+
+    // for (let i = 0; i < lista.length; i++){
+    //     sumalista += lista[i];
+    // }
+
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length
+        
+    return promedioLista   
 }
