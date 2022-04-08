280ece9d4d06555b4eda6267d23751f7a3fc98d9
Angel Vega <angelhidrobo2015@gmail.com>
2022-04-07 20:38:32 -0500

    big commit

---
+++ Course activities/salarios.js
@@ -0,0 +1,167 @@
+// const colombia = [];
+
+// colombia.push({
+//     name: "Miguel",
+//     salary: 500,
+// })
+// colombia.push({
+//     name: "Antonio",
+//     salary: 1750,
+// })
+// colombia.push({
+//     name: "Daniela",
+//     salary: 750,
+// })
+// colombia.push({
+//     name: "Martina",
+//     salary: 800,
+// })
+// colombia.push({
+//     name: "Jose",
+//     salary: 800,
+// })
+
+// colombia.push({
+//     name: "Kalev",
+//     salary: 650,
+// })
+// colombia.push({
+//     name: "Estefani",
+//     salary: 990,
+// })
+// colombia.push({
+//     name: "Juan",
+//     salary: 700,
+// })
+// colombia.push({
+//     name: "Marsela",
+//     salary: 900,
+// })
+// colombia.push({
+//     name: "Oliver",
+//     salary: 780,
+// })
+
+// colombia.push({
+//     name: "Eliseo",
+//     salary: 978,
+// })
+// colombia.push({
+//     name: "Maira",
+//     salary: 740,
+// })
+// colombia.push({
+//     name: "Moises",
+//     salary: 2000,
+// })
+// colombia.push({
+//     name: "Mateo",
+//     salary: 951,
+// })
+// colombia.push({
+//     name: "Pablo",
+//     salary: 750,
+// })
+
+// colombia.push({
+//     name: "Maribel",
+//     salary: 2500,
+// })
+// colombia.push({
+//     name: "Aaron",
+//     salary: 745,
+// })
+// colombia.push({
+//     name: "Matilde",
+//     salary: 888,
+// })
+// colombia.push({
+//     name: "Luisa",
+//     salary: 8514,
+// })
+// colombia.push({
+//     name: "Walter",
+//     salary: 780,
+// })
+
+const colombia = [];
+colombia.push({
+  name: "Camila",
+  salary: 500,
+});
+colombia.push({
+  name: "Nath",
+  salary: 1500,
+});
+colombia.push({
+  name: "Luisa",
+  salary: 1800,
+});
+colombia.push({
+  name: "Laura",
+  salary: 1000,
+});
+colombia.push({
+  name: "Daniela",
+  salary: 2200,
+});
+colombia.push({
+  name: "Esperancita",
+  salary: 200,
+});
+colombia.push({
+  name: "Carla",
+  salary: 500,
+});
+colombia.push({
+  name: "Antonieta",
+  salary: 1500,
+});
+colombia.push({
+  name: "Alicia",
+  salary: 1300,
+});
+colombia.push({
+  name: "Ana",
+  salary: 2400,
+});
+colombia.push({
+  name: "Julia",
+  salary: 3400,
+});
+colombia.push({
+  name: "Rosa",
+  salary: 400,
+});
+colombia.push({
+  name: "Angélica",
+  salary: 400,
+});
+colombia.push({
+  name: "Tatiana",
+  salary: 400,
+});
+colombia.push({
+  name: "Lorena",
+  salary: 600,
+});
+colombia.push({
+  name: "Carolina",
+  salary: 1600,
+});
+colombia.push({
+  name: "Fernanda",
+  salary: 2600,
+});
+colombia.push({
+  name: "Nora",
+  salary: 1000,
+});
+colombia.push({
+  name: "Gisselle",
+  salary: 2000,
+});
+colombia.push({
+  name: "Bill Gates",
+  salary: 100000000,
+});
